7d0f39d989a2fbd6163e5b039b374ae779ffb97f
Kareemnat12 <kareemnatsheh123@gmail.com>
2024-12-12 12:10:43 +0200

    changes
    ive changed the branch to check what will happen

--- test.py
+++ test.py
@@ -1,2 +1,3 @@
-print('hello world')
 print('this is the first branc im testing if it will cange the branche or not so im gonna check this ')
+
+print('in this stuff i deleted the first line and make a new one to check if it good or not ')
